149645a5f048e7236e5676a77da1bb6f09abe2d2
leguter <kolalevcenko80@gmail.com>
2024-08-17 12:35:41 -1200

    start project

---
+++ pages/HomePage/HomePage.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+const HomePage = () => {
+    const url =
+      "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1";
+
+    const options = {
+      headers: {
+        // Замість api_read_access_token вставте свій токен
+        Authorization: "Bearer api_read_access_token",
+      },
+    };
+
+    axios
+      .get(url, options)
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  return (
+    <h1>Trending today</h1>
+  )
+}
+
+export default HomePage
