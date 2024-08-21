2b84024f1774a4cae56d5c66854f488434789602
leguter <kolalevcenko80@gmail.com>
2024-08-21 10:17:54 -1200

    corect imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-
-import { Routes, Route} from "react-router-dom";
-import HomePage from '../pages/HomePage/HomePage';
-import MovieDetailsPage from '../pages/MovieDetailsPage/MovieDetailsPage';
-import MovieCast from './components/MovieCast/MovieCast';
-import MovieReviews from './components/MovieReviews/MovieReviews';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
+import MovieCast from "./components/MovieCast/MovieCast";
+import MovieReviews from "./components/MovieReviews/MovieReviews";
 import Navigation from "./components/Navigation/Navigation";
-import MoviesPage from "../pages/MoviesPage/MoviesPage";
+import MoviesPage from "./pages/MoviesPage/MoviesPage";
 function App() {
   return (
     <>
       <Navigation />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path='Movies'element={<MoviesPage />}/>
+        <Route path="Movies" element={<MoviesPage />} />
         <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
           <Route path="cast" element={<MovieCast />} />
           <Route path="reviews" element={<MovieReviews />} />
@@ -22,4 +21,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import MovieList from "../../components/MovieList/MovieList";
+const HomePage = () => {
+  const [TrendingMovies, setTrendingMovies] = useState([]);
+  useEffect(() => {
+    async function getMovies() {
+      const options = {
+        headers: {
+          // Замість api_read_access_token вставте свій токен
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YjZkNGU1ZTZmODYzN2RmNWM2YmYxZjgwZmRlZmIxMyIsIm5iZiI6MTcyMzkwNjI1NC40MjY3ODEsInN1YiI6IjY2YzBiMWI0MmVkZDU1YjRlZTkyMTgyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OFOO9NBVThnrE61EiyMKXRWS7W4VDKoWe-XvNrh3sU0",
+        },
+      };
+      const url = "https://api.themoviedb.org/3/trending/movie/day";
+      const response = await axios.get(url, options);
+      setTrendingMovies(response.data.results);
+    }
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      <h1>Trending today</h1>
+      <MovieList TrendingMovies={TrendingMovies} />
+    </div>
+  );
+};
+
+export default HomePage;
